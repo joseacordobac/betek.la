4a49eebd7d532f7a6f06c41c81e90b89572f2912
jose.cordoba <jose.cordoba@sinapsis.global>
2024-05-16 19:16:53 -0500

    update code for rdstation

--- footer.php
+++ footer.php
@@ -4,14 +4,13 @@
  * @package betek
  */
 ?>
-<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/bfb2a81d-51ca-4606-89d3-f8815f95ce96-loader.js" ></script>
-</body>
+
 
 <footer class="footer">
     <?php  get_template_part( 'organism/o-footer/o-footer', null, 
         array( 
             'class' => 'o-footer' 
-        ) ); ?>
+            ) ); ?>
 </footer>
-
+<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/bfb2a81d-51ca-4606-89d3-f8815f95ce96-loader.js" ></script>
 <?php wp_footer(); ?>
