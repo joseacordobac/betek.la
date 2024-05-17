8758f350db6b3546d0d74d1862ea2f0fdb309a8e
jose.cordoba <jose.cordoba@sinapsis.global>
2024-05-16 19:18:21 -0500

    update code for rdstation

--- footer.php
+++ footer.php
@@ -14,3 +14,4 @@
 </footer>
 <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/bfb2a81d-51ca-4606-89d3-f8815f95ce96-loader.js" ></script>
 <?php wp_footer(); ?>
+</body>
