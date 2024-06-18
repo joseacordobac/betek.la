9adb512f20655f328691f207222dc02ff6f53dcd
joseacordobac <joseacordobac@gmail.com>
2024-06-18 06:33:36 -0500

    update shoop details

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ include_once "templates/t-register.php";
 //Utilities
 include_once "utilities/u-register-styles.php";
 
-add_action('template_redirect', 'redirect_carrito_to_finalizar_compra');
+// add_action('template_redirect', 'redirect_carrito_to_finalizar_compra');
 function redirect_carrito_to_finalizar_compra() {
     if (is_page('carrito')) {
         wp_redirect(home_url('/finalizar-compra/'));
